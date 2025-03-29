c07fab06b9a6c2d7695197910a5dc645cb24c88b
Amir Dillard <dillard24a@ncssm.edu>
2025-03-29 11:20:03 -0400

    pull

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import FormPage from "./FormPage";
 import MatchingPage from "./MatchingPage";
@@ -8,7 +8,7 @@ import ProfilesPage from "./ProfilesPage";
 
 function App() {
   return (
-    <Router> { }
+    <Router> 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/form" element={<FormPage />} />
